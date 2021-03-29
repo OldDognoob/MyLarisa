8ee00d689117ea0fee778ff5e197d59ed6258431
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-03-29 21:33:51 +0300

    app-file

--- my-town/src/App.js
+++ my-town/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/style";
+import { makeStyles } from "@material-ui/core/styles";
+import { CssBaseline} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     minHeight: "100vh",
-    backgroundImage: ``,
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/images/larisa.jpg'})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
   },
@@ -13,7 +14,7 @@ export default function App() {
   const classes = useStyles();
     return(
       <div className={classes.root}>
-        {/* <CssBaseline/> */}
+        <CssBaseline/>
         {/* <Header/> */}
         {/* <PlaceToVisit/> */}
       </div>
